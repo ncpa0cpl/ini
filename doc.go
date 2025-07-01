4ea30a36edc529ff1c751de74618efbc7c49cadd
ncpa0cpl <szymonb21@gmail.com>
2025-07-01 12:24:30 +0200

    doc - store both kv fields and comments in a single array

--- doc.go
+++ doc.go
@@ -7,89 +7,77 @@ import (
 	"strings"
 )
 
+const (
+	lineTypeKv = iota
+	lineTypeComment
+	lineTypeHashComment
+)
+
 type FieldValue struct {
 	Key   string
 	Value string
 }
 
-type iniField struct {
-	order   int
-	key     string
-	value   string
-	comment string
-}
-
-type iniComment struct {
-	order       int
-	value       string
-	commentType string
+type iniLine struct {
+	lineType int
+	key      string
+	value    string
+	comment  string
 }
 
 type IniSection struct {
-	name         string
-	fields       []iniField
-	comments     []iniComment
-	comment      string
-	sectionOrder int
-	nextOrder    int
+	name    string
+	lines   []iniLine
+	comment string
 }
 
 type IniDoc struct {
-	fields           []iniField
-	comments         []iniComment
-	sections         []IniSection
-	nextFieldOrder   int
-	nextSectionOrder int
+	lines    []iniLine
+	sections []IniSection
 }
 
 func NewDoc() *IniDoc {
 	return &IniDoc{
-		fields:   []iniField{},
-		comments: []iniComment{},
+		lines:    []iniLine{},
 		sections: []IniSection{},
 	}
 }
 
-func (d *IniDoc) getField(key string) *iniField {
-	for idx := range d.fields {
-		if d.fields[idx].key == key {
-			return &d.fields[idx]
+func (d *IniDoc) getField(key string) *iniLine {
+	for idx := range d.lines {
+		if d.lines[idx].lineType == lineTypeKv && d.lines[idx].key == key {
+			return &d.lines[idx]
 		}
 	}
 	return nil
 }
 
 func (d *IniDoc) addField(key, value string) {
-	d.fields = append(d.fields, iniField{
-		key:   key,
-		value: value,
-		order: d.nextFieldOrder,
+	d.lines = append(d.lines, iniLine{
+		lineType: lineTypeKv,
+		key:      key,
+		value:    value,
 	})
-	d.nextFieldOrder++
 }
 
 func (d *IniDoc) AddComment(value string) {
-	d.comments = append(d.comments, iniComment{
-		order:       d.nextFieldOrder,
-		value:       value,
-		commentType: ";",
+	d.lines = append(d.lines, iniLine{
+		lineType: lineTypeComment,
+		value:    value,
 	})
-	d.nextFieldOrder++
 }
 
 func (d *IniDoc) AddHashComment(value string) {
-	d.comments = append(d.comments, iniComment{
-		order:       d.nextFieldOrder,
-		value:       value,
-		commentType: "#",
+	d.lines = append(d.lines, iniLine{
+		lineType: lineTypeHashComment,
+		value:    value,
 	})
-	d.nextFieldOrder++
 }
 
 func (d *IniDoc) Del(key string) {
-	for idx := range d.fields {
-		if d.fields[idx].key == key {
-			d.fields = slices.Delete(d.fields, idx, idx+1)
+	for idx := range d.lines {
+		if d.lines[idx].lineType == lineTypeKv && d.lines[idx].key == key {
+			d.lines = slices.Delete(d.lines, idx, idx+1)
 			return
 		}
 	}
@@ -183,29 +171,26 @@ func (d *IniDoc) Section(sectionName string) *IniSection {
 	}
 
 	section := IniSection{
-		name:         sectionName,
-		fields:       []iniField{},
-		comments:     []iniComment{},
-		sectionOrder: d.nextSectionOrder,
+		name:  sectionName,
+		lines: []iniLine{},
 	}
-	d.nextSectionOrder++
 
 	d.sections = append(d.sections, section)
 	return &d.sections[len(d.sections)-1]
 }
 
 func (d *IniDoc) Keys() []string {
-	keys := make([]string, len(d.fields))
-	for idx := range d.fields {
-		keys[idx] = d.fields[idx].key
+	keys := make([]string, len(d.lines))
+	for idx := range d.lines {
+		keys[idx] = d.lines[idx].key
 	}
 	return keys
 }
 
 func (d *IniDoc) Values() []FieldValue {
-	keys := make([]FieldValue, len(d.fields))
-	for idx := range d.fields {
-		keys[idx] = FieldValue{d.fields[idx].key, d.fields[idx].value}
+	keys := make([]FieldValue, len(d.lines))
+	for idx := range d.lines {
+		keys[idx] = FieldValue{d.lines[idx].key, d.lines[idx].value}
 	}
 	return keys
 }
@@ -218,46 +203,41 @@ func (d *IniDoc) SectionNames() []string {
 	return names
 }
 
-func (d *IniSection) getField(key string) *iniField {
-	for idx := range d.fields {
-		if d.fields[idx].key == key {
-			return &d.fields[idx]
+func (d *IniSection) getField(key string) *iniLine {
+	for idx := range d.lines {
+		if d.lines[idx].lineType == lineTypeKv && d.lines[idx].key == key {
+			return &d.lines[idx]
 		}
 	}
 	return nil
 }
 
 func (d *IniSection) addField(key, value string) {
-	d.fields = append(d.fields, iniField{
-		key:   key,
-		value: value,
-		order: d.nextOrder,
+	d.lines = append(d.lines, iniLine{
+		lineType: lineTypeKv,
+		key:      key,
+		value:    value,
 	})
-	d.nextOrder++
 }
 
 func (d *IniSection) AddComment(value string) {
-	d.comments = append(d.comments, iniComment{
-		order:       d.nextOrder,
-		value:       value,
-		commentType: ";",
+	d.lines = append(d.lines, iniLine{
+		lineType: lineTypeComment,
+		value:    value,
 	})
-	d.nextOrder++
 }
 
 func (d *IniSection) AddHashComment(value string) {
-	d.comments = append(d.comments, iniComment{
-		order:       d.nextOrder,
-		value:       value,
-		commentType: "#",
+	d.lines = append(d.lines, iniLine{
+		lineType: lineTypeHashComment,
+		value:    value,
 	})
-	d.nextOrder++
 }
 
 func (d *IniSection) Del(key string) {
-	for idx := range d.fields {
-		if d.fields[idx].key == key {
-			d.fields = slices.Delete(d.fields, idx, idx+1)
+	for idx := range d.lines {
+		if d.lines[idx].lineType == lineTypeKv && d.lines[idx].key == key {
+			d.lines = slices.Delete(d.lines, idx, idx+1)
 			return
 		}
 	}
@@ -344,84 +324,49 @@ func (d *IniSection) GetBool(key string) (bool, error) {
 }
 
 func (d *IniSection) Keys() []string {
-	keys := make([]string, len(d.fields))
-	for idx := range d.fields {
-		keys[idx] = d.fields[idx].key
+	keys := make([]string, len(d.lines))
+	for idx := range d.lines {
+		keys[idx] = d.lines[idx].key
 	}
 	return keys
 }
 
 func (d *IniSection) Values() []FieldValue {
-	keys := make([]FieldValue, len(d.fields))
-	for idx := range d.fields {
-		keys[idx] = FieldValue{d.fields[idx].key, d.fields[idx].value}
+	keys := make([]FieldValue, len(d.lines))
+	for idx := range d.lines {
+		keys[idx] = FieldValue{d.lines[idx].key, d.lines[idx].value}
 	}
 	return keys
 }
 
 // serialization
 
-func (f *iniField) ToString() string {
-	v := fmt.Sprintf("%s=%s", f.key, f.value)
-	if f.comment != "" {
-		v += fmt.Sprintf(" ;%s", f.comment)
-	}
-	v = strings.ReplaceAll(v, "\n", "\\n")
-	return v + "\n"
-}
-
-func (c *iniComment) ToString() string {
-	var v string
-
-	lines := strings.Split(c.value, "\n")
-	if c.commentType == ";" {
+func (f *iniLine) ToString() string {
+	switch f.lineType {
+	case lineTypeKv:
+		v := fmt.Sprintf("%s=%s", f.key, f.value)
+		if f.comment != "" {
+			v += fmt.Sprintf(" ;%s", f.comment)
+		}
+		v = strings.ReplaceAll(v, "\n", "\\n")
+		return v + "\n"
+	case lineTypeComment:
+		var v string
+		lines := strings.Split(f.value, "\n")
 		for _, line := range lines {
 			v = fmt.Sprintf("; %s\n", line)
 		}
-	} else {
+		return v
+	case lineTypeHashComment:
+		var v string
+		lines := strings.Split(f.value, "\n")
 		for _, line := range lines {
 			v = fmt.Sprintf("# %s\n", line)
 		}
+		return v
 	}
 
-	return v
-}
-
-func (s *IniSection) getNextLine(prevOrder int) (line string, orderNo int, done bool) {
-	var lowestField *iniField
-	var lowestComment *iniComment
-
-	for idx := range s.fields {
-		if s.fields[idx].order > prevOrder {
-			lowestField = &s.fields[idx]
-			break
-		}
-	}
-
-	for idx := range s.comments {
-		if s.comments[idx].order > prevOrder {
-			lowestComment = &s.comments[idx]
-			break
-		}
-	}
-
-	if lowestField == nil && lowestComment == nil {
-		return "", 0, true
-	}
-
-	if lowestComment == nil {
-		return lowestField.ToString(), lowestField.order, false
-	}
-
-	if lowestField == nil {
-		return lowestComment.ToString(), lowestComment.order, false
-	}
-
-	if lowestField.order < lowestComment.order {
-		return lowestField.ToString(), lowestField.order, false
-	} else {
-		return lowestComment.ToString(), lowestComment.order, false
-	}
+	panic("invalid line type: " + strconv.FormatInt(int64(f.lineType), 10))
 }
 
 func (s *IniSection) ToString() string {
@@ -433,81 +378,18 @@ func (s *IniSection) ToString() string {
 
 	v += fmt.Sprintf("[%s]\n", s.name)
 
-	slices.SortFunc(s.fields, func(a, b iniField) int {
-		return a.order - b.order
-	})
-	slices.SortFunc(s.comments, func(a, b iniComment) int {
-		return a.order - b.order
-	})
-
-	var nextline string
-	var prevOrder int = -1
-	var done bool
-	for !done {
-		nextline, prevOrder, done = s.getNextLine(prevOrder)
-		if nextline != "" {
-			v += nextline
-		}
+	for _, line := range s.lines {
+		v += line.ToString()
 	}
 
 	return v
 }
 
-func (d *IniDoc) getNextLine(prevOrder int) (line string, orderNo int, done bool) {
-	var lowestField *iniField
-	var lowestComment *iniComment
-
-	for idx := range d.fields {
-		if d.fields[idx].order > prevOrder {
-			lowestField = &d.fields[idx]
-			break
-		}
-	}
-
-	for idx := range d.comments {
-		if d.comments[idx].order > prevOrder {
-			lowestComment = &d.comments[idx]
-			break
-		}
-	}
-
-	if lowestField == nil && lowestComment == nil {
-		return "", 0, true
-	}
-
-	if lowestComment == nil {
-		return lowestField.ToString(), lowestField.order, false
-	}
-
-	if lowestField == nil {
-		return lowestComment.ToString(), lowestComment.order, false
-	}
-
-	if lowestField.order < lowestComment.order {
-		return lowestField.ToString(), lowestField.order, false
-	} else {
-		return lowestComment.ToString(), lowestComment.order, false
-	}
-}
-
 func (d *IniDoc) ToString() string {
 	var v string = ""
 
-	slices.SortFunc(d.fields, func(a, b iniField) int {
-		return a.order - b.order
-	})
-	slices.SortFunc(d.comments, func(a, b iniComment) int {
-		return a.order - b.order
-	})
-
-	var nextline string
-	var prevOrder int = -1
-	var done bool
-	for !done {
-		nextline, prevOrder, done = d.getNextLine(prevOrder)
-		if nextline != "" {
-			v += nextline
-		}
+	for _, line := range d.lines {
+		v += line.ToString()
 	}
 
 	for _, section := range d.sections {
